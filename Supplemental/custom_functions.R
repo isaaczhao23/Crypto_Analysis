c35063d21ea3a4d7188c4a22333e9414cf40f7b9
Isaac Zhao <isaaczhao23@yahoo.com>
2018-05-22 22:45:56 -0400

    Made a better downtrend prediction model

--- Supplemental/custom_functions.R
+++ Supplemental/custom_functions.R
@@ -1,5 +1,5 @@
 source("check_packages.R")
-check_packages(c("MASS","plyr","tidyverse", "tidyr","scales", "crypto", "ggthemes", "directlabels", "shiny","ggpubr", "ggrepel","readxl","gridExtra","sjPlot","lubridate"))
+check_packages(c("MASS","plyr","RcmdrMisc", "tidyverse", "tidyr","scales", "crypto", "ggthemes", "directlabels", "shiny","ggpubr", "ggrepel","readxl","gridExtra","sjPlot","lubridate"))
 
 ###############################################################################################
 ###############################################################################################
@@ -13,6 +13,41 @@ percentage = function(x){
 
 ###############################################################################################
 ###############################################################################################
+
+model_fit_stats <- function(linear.model) {
+  r.sqr <- summary(linear.model)$r.squared
+  adj.r.sqr <- summary(linear.model)$adj.r.squared
+  pre.r.sqr <- pred_r_squared(linear.model)
+  PRESS <- PRESS(linear.model)
+  return.df <- data.frame(r.squared = r.sqr, adj.r.squared = adj.r.sqr, pred.r.squared = pre.r.sqr, press = PRESS)
+  return(return.df)
+}
+
+pred_r_squared <- function(linear.model) {
+  #' Use anova() to get the sum of squares for the linear model
+  lm.anova <- anova(linear.model)
+  #' Calculate the total sum of squares
+  tss <- sum(lm.anova$'Sum Sq')
+  # Calculate the predictive R^2
+  pred.r.squared <- 1-PRESS(linear.model)/(tss)
+  
+  return(pred.r.squared)
+}
+
+
+PRESS <- function(linear.model) {
+  #' calculate the predictive residuals
+  pr <- residuals(linear.model)/(1-lm.influence(linear.model)$hat)
+  #' calculate the PRESS
+  PRESS <- sum(pr^2)
+  
+  return(PRESS)
+}
+
+# use ldply(list(fit1, fit2), model_fit_stats) to compare predictive accuracy
+###############################################################################################
+###############################################################################################
+
 
 generate_coin.list <- function(comparison_date, end_date, topranks = 30){
     
@@ -30,6 +65,44 @@ generate_coin.list <- function(comparison_date, end_date, topranks = 30){
     } else {
             coin.list = getCoins(limit= topranks, start_date = end_date1, end_date = comparison_date1) %>%   dplyr::select(name,date,close) %>% dplyr::rename(price = close)
     }
+}
+
+###############################################################################################
+###############################################################################################
+# Used for creating btc prediction dataframe. 
+# Needs btc.df (list of all dates with all btc values) to run this function
+
+regression = function(start.date,end.date,btc.df) {
+n = length(start.date)
+output = rep(NA,n)
+
+for (i in 1:n){
+	if (is.na(start.date[i]) || is.na(end.date[i])){
+	output[i] = NA
+	} else {
+   price.df = btc.df %>% filter(date <= end.date[i], date >= start.date[i] ) %>% dplyr::select(price) 	
+          days = seq(from=1,to=nrow(price.df),by=1)
+          reg.df = cbind(price.df,days)
+          output[i] = lm(price~days,reg.df)$coefficients[[2]] / reg.df$price[1]  *100
+	   }
+}
+return(output)
+}
+
+
+
+
+bad.months.function = function(date) {
+	output = rep(NA,length(date))
+	for (i in 1:length(date)){
+	month1 = as.numeric(month(date[i]))
+	if ( (month1 >=1 && month1 < 3) || month1 >= 11   ){
+	output[i] = 1
+	} else{
+   output[i] = 0
+	}
+	}
+return(as.factor(output))
 }
 
 
@@ -177,43 +250,96 @@ comparison_graph = function(comparison_date = "2018-02-06", end_date = Sys.Date(
 ###############################################################################################
 ###############################################################################################
 
-predict_correction = function(oath.date, oatl.date, ath.date, atl.date = Sys.Date()-1){
-
+predict_correction = function(ooath.date, ooatl.date,oath.date, oatl.date, ath.date, atl.date = Sys.Date()-1){
+	
+ooath.date = "2018-01-06"
+ooatl.date = "2018-02-05"
+oath.date = "2018-03-04"
+oatl.date = "2018-04-06"
+ath.date = "2018-05-05"
+atl.date = Sys.Date()
+	
+	
+ooath.date = as.Date(ooath.date)
+ooatl.date = as.Date(ooatl.date)
 oath.date = as.Date(oath.date)
 oatl.date = as.Date(oatl.date)
 ath.date = as.Date(ath.date)
-today.date = atl.date
+atl.date = as.Date(atl.date)
 
-load("fit1.Rda")
-btc_2018 = getCoins(coin="Bitcoin",start_date='20171201') %>% dplyr::rename(price = close) %>% dplyr::select(date,price)
+load("downtrend_model2.Rda")
+btc.df = getCoins(coin="Bitcoin",start_date='20180101') %>% dplyr::rename(price = close) %>% dplyr::select(date,price)
 
-pred.df = data.frame(oath.date,oatl.date,ath.date,today.date)
+pred.df2 = data.frame(ooath.date,ooatl.date,oath.date,oatl.date,ath.date,atl.date)
 
-a1 = btc_2018 %>% dplyr::rename(oath.date = date) %>% dplyr::rename(oath = price)
-a2 = btc_2018 %>% dplyr::rename(oatl.date = date) %>% dplyr::rename(oatl = price)
-a3 = btc_2018 %>% dplyr::rename(ath.date = date) %>% dplyr::rename(ath = price)
-a4 = btc_2018 %>% dplyr::rename(today.date = date) %>% dplyr::rename(atl = price)
+a1 = btc.df %>% dplyr::rename(ooath.date = date) %>% dplyr::rename(ooath = price)
+a2 = btc.df %>% dplyr::rename(ooatl.date = date) %>% dplyr::rename(ooatl = price)
+a3 = btc.df %>% dplyr::rename(oath.date = date) %>% dplyr::rename(oath = price)
+a4 = btc.df %>% dplyr::rename(oatl.date = date) %>% dplyr::rename(oatl = price)
+a5 = btc.df %>% dplyr::rename(ath.date = date) %>% dplyr::rename(ath = price)
+a6 = btc.df %>% dplyr::rename(atl.date = date) %>% dplyr::rename(atl = price)
 
-pred.df = left_join(pred.df,a1,by="oath.date") %>% left_join(a2,by="oatl.date") %>% left_join(a3,by="ath.date") %>% left_join(a4,by="today.date") %>% mutate(days.oath.oatl = as.numeric(oatl.date - oath.date)) %>% mutate(pct.oath.oatl = (oath-oatl)/oath * 100) %>% mutate(pct.day.oath.oatl = pct.oath.oatl/days.oath.oatl)
+pred.df = pred.df2 %>%
+	left_join(a1,by="ooath.date") %>% 
+	left_join(a2,by="ooatl.date") %>% 
+	left_join(a3,by="oath.date") %>% 
+	left_join(a4,by="oatl.date") %>% 
+	left_join(a5,by="ath.date") %>% 
+	left_join(a6,by="atl.date")
 
-pred.df$reg.ath.atl = NA
-numbers = 1
-i=1
-prices = btc_2018 %>% filter(date <= pred.df[[i,"today.date"]], date >= pred.df[[i,"ath.date"]]) %>% dplyr::select(price) 
-if (nrow(prices) == 0 ){
-	df$reg.ath.atl[i] = NA
-} else {
-numbers = seq(from=1,to=nrow(prices),by=1)
-reg.df = cbind(prices,numbers)
-pred.df$reg.ath.atl = -(lm(price~numbers,reg.df)$coefficients[[2]] / pred.df[[i,"ath"]])*100
-}
+# Create percent change of uptrend/downtrend
+pred.df$pct.ooath.ooatl = (pred.df$ooath - pred.df$ooatl)/pred.df$ooath * 100
+pred.df$pct.ooatl.oath = (pred.df$oath - pred.df$ooatl)/pred.df$ooatl * 100
+pred.df$pct.oath.oatl = (pred.df$oath - pred.df$oatl)/pred.df$oath * 100
+pred.df$pct.oatl.ath = (pred.df$ath - pred.df$oatl)/pred.df$oatl * 100
+pred.df$pct.ooath.oath = (pred.df$oath - pred.df$ooath)/pred.df$ooath * 100
+pred.df$pct.oath.ath = (pred.df$ath - pred.df$oath)/pred.df$oath * 100
+pred.df$pct.ooatl.oatl = (pred.df$oatl - pred.df$ooatl)/pred.df$ooatl * 100
+
+# Create duration of days
+pred.df$days.ooath.ooatl = as.numeric(pred.df$ooatl.date - pred.df$ooath.date)
+pred.df$days.ooatl.oath = as.numeric(pred.df$oath.date - pred.df$ooatl.date)
+pred.df$days.oath.oatl = as.numeric(pred.df$oatl.date - pred.df$oath.date)
+pred.df$days.oatl.ath = as.numeric(pred.df$ath.date - pred.df$oatl.date)
+pred.df$days.ooath.oath = as.numeric(pred.df$oath.date - pred.df$ooath.date)
+pred.df$days.oath.ath = as.numeric(pred.df$ath.date - pred.df$oath.date)
+pred.df$days.ooatl.oatl = as.numeric(pred.df$oatl.date - pred.df$ooatl.date)
+
+# Create percent per day
+pred.df$pct.day.ooath.ooatl = pred.df$pct.ooath.ooatl/pred.df$days.ooath.ooatl
+pred.df$pct.day.ooatl.oath = pred.df$pct.ooatl.oath/pred.df$days.ooatl.oath
+pred.df$pct.day.oath.oatl = pred.df$pct.oath.oatl/pred.df$days.oath.oatl
+pred.df$pct.day.oatl.ath = pred.df$pct.oatl.ath/pred.df$days.oatl.ath
+pred.df$pct.day.ooath.oath = pred.df$pct.ooath.oath/pred.df$days.ooath.oath
+pred.df$pct.day.oath.ath = pred.df$pct.oath.ath/pred.df$days.oath.ath
+pred.df$pct.day.ooatl.oatl = pred.df$pct.ooatl.oatl/pred.df$days.ooatl.oatl
 
 
-fit1_conf_ci = exp(predict(fit1, pred.df, se.fit=T, type='response',interval = "conf")$fit)
-fit1_pred_ci = exp(predict(fit1, pred.df, se.fit=T, type='response',interval = "pred")$fit)
+# Create regression
+pred.df$reg.ooath.ooatl = abs(regression(start.date = pred.df$ooath.date,end.date = pred.df$ooatl.date, btc.df))
+pred.df$reg.ooatl.oath = abs(regression(start.date = pred.df$ooatl.date,end.date = pred.df$oath.date,btc.df))
+pred.df$reg.oath.oatl = abs(regression(start.date = pred.df$oath.date,end.date = pred.df$oatl.date,btc.df))
+pred.df$reg.oatl.ath = abs(regression(start.date = pred.df$oatl.date,end.date = pred.df$ath.date,btc.df))
+pred.df$reg.ath.atl = abs(regression(start.date = pred.df$ath.date,end.date = pred.df$atl.date,btc.df))
+pred.df$reg.ooath.oath =  regression(start.date = pred.df$ooath.date,end.date = pred.df$oath.date,btc.df)
+pred.df$reg.oath.ath = regression(start.date = pred.df$oath.date,end.date = pred.df$ath.date,btc.df)
+pred.df$reg.ooatl.oatl = regression(start.date = pred.df$ooatl.date,end.date = pred.df$oatl.date,btc.df)
+
+pred.df$bad.months = bad.months.function(pred.df$ath.date)
+
+
+# Creates factor if ath is less than oath
+pred.df$ath.bull = as.factor(ifelse(pred.df$ath < pred.df$oath, 0,1))
+
+
+fit1_conf_ci = exp(predict(downtrend_model2, pred.df, se.fit=T, type='response',interval = "conf",level=0.68)$fit)
 fit1_conf_ci_low = as.Date(ath.date) + fit1_conf_ci[2] 
 fit1_conf_ci_mean = as.Date(ath.date) + fit1_conf_ci[1]
 fit1_conf_ci_high = as.Date(ath.date) + fit1_conf_ci[3]
 
-output = print(paste("Correction is expected to end around", format(fit1_conf_ci_low,"%B %d, %Y"), "to", format(fit1_conf_ci_high,"%B %d, %Y"),"with a mean expected date of", format(fit1_conf_ci_mean,"%B %d, %Y")))
+print(paste("Downtrend will end somewhere between", format(fit1_conf_ci_low,"%B %d, %Y"), 
+	"and", format(fit1_conf_ci_high,"%B %d, %Y"), "with an expected date of", format(fit1_conf_ci_mean,"%B %d, %Y"),
+	", which is",round(fit1_conf_ci[1]),"days total since the beginning of the downtrend."))
+	
+print(paste0("This downtrend drops about $", round(pred.df$ath*pred.df$reg.ath.atl/100), " per day."))
 }
